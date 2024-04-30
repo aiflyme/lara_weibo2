00f63f855c6dbfeac538498058348941491bb8be
Robin Chen <aiflyme@gmail.com>
2024-04-30 16:56:24 +0200

    data model

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -15,11 +15,11 @@ class PasswordController extends Controller
     public function __construct()
     {
         //Access limit  2 times 1 minute
-        $this->middleware('throttle:2,1',[
+        $this->middleware('throttle:20,1',[
            'only' => ['showLinkRequestForm']
         ]);
 
-        $this->middleware('throttle:3,10',[
+        $this->middleware('throttle:30,10',[
            'only' => ['sendResetLinkEmail'],
         ]);
     }
@@ -86,11 +86,24 @@ class PasswordController extends Controller
         $email = $request->email;
         $token = $request->token;
 
-        $expires = 60 * 10;
+        $expires = 60 * 60;
+
+//        echo $request->token;
+//        echo '<br>';
+//        $hashToken = hash::make($request->token);
+//
+//        echo $hashToken;
+//        echo '<br>';
+//        echo Hash::check($request->token, $hashToken);
         //2. get user from db
-        $user = User::where([
-            'email'=> $email,
-        ])->firstOrFail();
+//        $user = User::where([
+//            'email'=> $email,
+//        ])->firstOrFail();
+        $user = (array)DB::table('users as u')
+            ->leftJoin('password_reset_tokens as prt', 'u.email', '=', 'prt.email')
+            ->select('u.email', 'prt.token','prt.created_at')
+            ->where('u.email','=', $email)
+            ->first();
 
         //3.if not exist
         if(is_null($user)){
@@ -99,25 +112,30 @@ class PasswordController extends Controller
         }
 
         //4.read the password_resets data
-        $record = (array)DB::table('password_reset_tokens')->where('email', $email)->first();
+        //$record = (array)DB::table('password_reset_tokens')->where('email', $email)->first();
 
         //find the data
-        if($record){
+        if($user){
             //5.1 expire
-            if(Carbon::parse($record['created_at'])->addSecond($expires)->isPast()){
+            if(Carbon::parse($user['created_at'])->addSecond($expires)->isPast()){
                 session()->flash('danger', '链接已过期，请重新尝试');
                 return redirect()->back();
             }
 
             //5.2.token is right
-            if(!Hash::check($token, $record['token'])){
+            if(!Hash::check($token, $user['token'])){
                 session()->flash('danger', '令牌错误');
                 return redirect()->back();
             }
             //5.3 update password
-            $user->update([
+            User::where('email',$email)->update([
                 'password' => bcrypt($request->password),
             ]);
+//            DB::table('users')
+//                ->where('email',$email)
+//                ->update(['password' => bcrypt($request->password)]);
+
+
             // 5.4. 提示用户更新成功
             session()->flash('success', '密码重置成功，请使用新密码登录');
             return redirect()->route('login');
